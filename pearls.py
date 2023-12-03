e88a82a07938121c890b288a45edb065188c0a8c
Pearllatras <152365463+Pearllatras@users.noreply.github.com>
2023-12-03 15:57:34 +0800

    Update pearls.py

--- pearls.py
+++ pearls.py
@@ -1,7 +1,18 @@
+import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
 
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+def load_lottieurl(url)
+r = requests.get(url)
+ if r.status_code != 200:
+   return None
+  return r.json()
+
+# ---- LOAD ASSETS ----
+Lottie_coding= load_lottieurl"https://app.lottiefiles.com/animation/571d91e8-a4f0-4f44-9176-da69a0cdb146"
 
 # ---- HEADER SECTION ----
 with st.container():
@@ -25,4 +36,5 @@ with st.container():
         """
     )
     st.write("My Github Account >](https://github.com/Pearllatras/pearllatras/edit/main/pearls.py")
-    
+    with rigth_column
+    st.lottie(lottie_coding, height=400, key="coding")
