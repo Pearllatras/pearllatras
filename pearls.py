cb31383849bae8cc86b74d9bb78e8130735a0fd2
Pearllatras <152365463+Pearllatras@users.noreply.github.com>
2023-12-03 14:37:53 +0800

    Update pearls.py

--- pearls.py
+++ pearls.py
@@ -1,2 +1,22 @@
 import streamlit as st
-st.write("hello")
+st.set_pge_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+with st.container():
+  st.subheader("Hi, I am Pearl :wave:")
+  st.title("A Computer Engineering Student from SNSU")
+  st.write("I am passionate to learn more about this amazing course")
+
+with st.container():
+  st.write("---")
+  left_column, right_column = st.columns(2)
+  with left_column:
+    st.header("What I like")
+    st.write("##")
+    st.write(
+        """
+       * DRAWING
+       * WATCHING ANIME/KDRAMAS
+       * SWIMMING
+       * ROAD TRIP/ISLAND HOPPING
+        """
+    )
+    
