3c6e1debf4a42855807fa0f2b65d64ae746e7b6d
Pearllatras <152365463+Pearllatras@users.noreply.github.com>
2023-12-10 01:28:07 +0800

    Update pearls.py

--- pearls.py
+++ pearls.py
@@ -12,29 +12,30 @@ def load_lottieurl(url):
   return r.json()
 
 # ---- LOAD ASSETS ----
-lottie_coding = load_lottieurl("https://lottie.host/f55fae42-4cd9-410b-8be2-911c8823610e/L8JGXaVJsA.json")
+lottie_coding = load_lottieurl("https://lottie.host/65dd4082-786e-45fc-b781-818d5fa0f6dc/PtqocH9wII.json")
 
 # ---- HEADER SECTION ----
 with st.container():
   st.subheader("Hi, I am Pearl :wave:")
   st.title("A Computer Engineering Student from SNSU")
-  st.write("I am here to learn more about this amazing course with my co-student. And with the help of m instructure and also the tutorial on yt I made my own webpage with my own hard word and knowlegde")
-
+  st.write("I am here to learn more about this amazing course with my co-student. And with the help of my instructure and also the tutorial on yt I made my own webpage with my own hard word and knowlegde.")
+  
 # ---- WHAT I DO ----
 with st.container():
   st.write("---")
   left_column, right_column = st.columns(2)
   with left_column:
-    st.header("What I love :heart:")
+    st.header("What you need to know")
     st.write("##")
     st.write(
         """
-       * I love to draw since i was in young age
-       * I also loved watching cartoons it makes me feel like I am over the moon and it can keep my stress away
-       * I like swimming too, unwinding, bonding with friends, and also having a me time at the same time
-       * Have some quality time with my family was the most precious thing it could be happen
-       * And then i also loved exploring new things specially if i can get new learnings from those things
+       * I am Pearl Marie J. Latras
+       * 18 years old born on the second day of April 2005
+       * My both parents was a pure Filipino
+       * My father was a  farmer and my mother was also a house wife
+       * I have a 3 siblings and I am the 3rd one
+       * I also love to draw, exploring new things specially when I can get some knowledge
         """
     )   
 with right_column:
-  st_lottie(lottie_coding, height=400, key="coding")
+  st_lottie(lottie_coding, height=300, key="coding")
