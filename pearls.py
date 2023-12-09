be1b64640563df90a94167ffa5e2dd4fe9ed3409
Pearllatras <152365463+Pearllatras@users.noreply.github.com>
2023-12-10 00:34:36 +0800

    Update pearls.py

--- pearls.py
+++ pearls.py
@@ -18,22 +18,22 @@ lottie_coding = load_lottieurl("https://lottie.host/f55fae42-4cd9-410b-8be2-911c
 with st.container():
   st.subheader("Hi, I am Pearl :wave:")
   st.title("A Computer Engineering Student from SNSU")
-  st.write("I am here to learn more about this amazing course with my co-student")
+  st.write("I am here to learn more about this amazing course with my co-student. And with the help of m instructure and also the tutorial on yt I made my own webpage with my own hard word and knowlegde")
 
 # ---- WHAT I DO ----
 with st.container():
   st.write("---")
   left_column, right_column = st.columns(2)
   with left_column:
-    st.header("What I like")
+    st.header("What I love :heart:")
     st.write("##")
     st.write(
         """
-       * DRAWING
-       * WATCHING ANIME/KDRAMAS
-       * SWIMMING
-       * ROAD TRIP/ISLAND HOPPING
-       * EXPLORING ABOUT NEW THINGS
+       * I love to draw since i was in young age
+       * I also loved watching cartoons it makes me feel like I am over the moon and it can keep my stress away
+       * I like swimming too, unwinding, bonding with friends, and also having a me time at the same time
+       * Have some quality time with my family was the most precious thing it could be happen
+       * And then i also loved exploring new things specially if i can get new learnings from those things
         """
     )   
 with right_column:
