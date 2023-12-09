eda19918253d431e83dd415241539f9ef87fe82d
Pearllatras <152365463+Pearllatras@users.noreply.github.com>
2023-12-10 00:05:37 +0800

    Update pearls.py

--- pearls.py
+++ pearls.py
@@ -1,8 +1,18 @@
+import requests
 import streamlit as st
-
+from streamlit_lottie import st_lottie
 
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+def load_lottieurl(url):
+  r = requests.get(url)
+  if r.status_code != 200:
+      return None
+  return r.json()
+
+# ---- LOAD ASSETS ----
+lottie_coding = load_lottieurl("https://lottie.host/f55fae42-4cd9-410b-8be2-911c8823610e/L8JGXaVJsA.json")
 
 # ---- HEADER SECTION ----
 with st.container():
@@ -26,4 +36,5 @@ with st.container():
        * EXPLORING ABOUT NEW THINGS
         """
     )   
-  
+with right_column:
+  st_lottie(lottie_coding, height=400, key="coding")
